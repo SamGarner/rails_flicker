20d7bad634c4d9815644cc0a2f8ac5c52edc02bb
SamGarner <samuel.w.garner@gmail.com>
2021-01-23 13:19:33 -0600

    Feat: Update StaticPagesController to use Figaro gem for API key/secret rather than ENV variables from .bashrc

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,8 +7,10 @@ class StaticPagesController < ApplicationController
       render 'index'
     else
       # API
-      flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_API_SECRET'])
-      @photos = flickr.photos.search(api_key: ENV['FLICKR_API_KEY'], user_id: params[:user_id])
+      flickr = Flickr.new(Figaro.env.flickr_api_key, Figaro.env.flickr_api_secret)
+      @photos = flickr.photos.search(api_key: Figaro.env.flickr_api_key, user_id: params[:user_id])
+      # flickr = Flickr.new(ENV['FLICKR_API_KEY'], ENV['FLICKR_API_SECRET'])
+      # @photos = flickr.photos.search(api_key: ENV['FLICKR_API_KEY'], user_id: params[:user_id])
 
       render 'index'
     end
